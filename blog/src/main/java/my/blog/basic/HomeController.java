a7b41b76d9beb1943c94e41fd7a2dc411bfb0743
StartDeveloperKim <ktw550111@naver.com>
2023-01-04 22:45:58 +0900

    Fix. HomeController RestController 수정

--- blog/src/main/java/my/blog/basic/HomeController.java
+++ blog/src/main/java/my/blog/basic/HomeController.java
@@ -10,30 +10,24 @@ import my.blog.user.dto.SessionUser;
 import my.blog.user.dto.UserInfo;
 import my.blog.user.service.LoginUser;
 import my.blog.web.layout.LayoutService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 import java.util.Map;
 
 @Slf4j
 @RequiredArgsConstructor
-@Controller
+@RestController
 public class HomeController {
 
     private final BoardLookupService boardLookupService;
-    private final LayoutService layoutService;
 
     @GetMapping("/")
-    public String homeForm(@LoginUser SessionUser user, Model model) {
-        layoutService.getLayoutInfo(model);
-        model.addAttribute("boardList", boardLookupService.getBoardListRecent());
-
-        if (user != null) {
-            model.addAttribute("userInfo", new UserInfo(user.getUserId(), user.getName()));
-        }
-
-        return "index";
+    public ResponseEntity<List<BoardResponse>> homeForm() {
+        return ResponseEntity.ok(boardLookupService.getBoardListRecent());
     }
 }
